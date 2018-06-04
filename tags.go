d4df4b4a821bf8378f8b731124ae7e9149385385
Marcus Willock <crazcalm@gmail.com>
2018-06-04 14:41:54 -0400

    Minor error fix. Changed tags like <h2> to <h2 so that my parser picks them up

--- tags.go
+++ tags.go
@@ -19,32 +19,32 @@ const (
 	CloseDivTag = Tag("</div>")
 
 	//OpenH1Tag -- Open html h1 tag
-	OpenH1Tag = Tag("<h1>")
+	OpenH1Tag = Tag("<h1")
 	//CloseH1Tag -- Closed html h1 tag
 	CloseH1Tag = Tag("</h1>")
 
 	//OpenH2Tag -- Open html h2 tag
-	OpenH2Tag = Tag("<h2>")
+	OpenH2Tag = Tag("<h2")
 	//CloseH2Tag -- Close html h2 tag
 	CloseH2Tag = Tag("</h2>")
 
 	//OpenH3Tag -- Open html h3 tag
-	OpenH3Tag = Tag("<h3>")
+	OpenH3Tag = Tag("<h3")
 	//CloseH3Tag -- Close html h3 tag
 	CloseH3Tag = Tag("</h3>")
 
 	//OpenH4Tag -- Open html h4 tag
-	OpenH4Tag = Tag("<h4>")
+	OpenH4Tag = Tag("<h4")
 	//CloseH4Tag -- Close html h4 tag
 	CloseH4Tag = Tag("</h4>")
 
 	//OpenH5Tag -- Open html h5 tag
-	OpenH5Tag = Tag("<h5>")
+	OpenH5Tag = Tag("<h5")
 	//CloseH5Tag -- Close html h5 tag
 	CloseH5Tag = Tag("</h5>")
 
 	//OpenH6Tag -- Open html h6 tag
-	OpenH6Tag = Tag("<h6>")
+	OpenH6Tag = Tag("<h6")
 	//CloseH6Tag -- Close html h6 tag
 	CloseH6Tag = Tag("</h6>")
 
@@ -79,7 +79,7 @@ const (
 	CloseTableTag = Tag("</table>")
 
 	//OpenTRTag -- Open html tr tag
-	OpenTRTag = Tag("<tr>")
+	OpenTRTag = Tag("<tr")
 	//CloseTRTag -- Close html tr tag
 	CloseTRTag = Tag("</tr>")
 
@@ -94,7 +94,7 @@ const (
 	CloseTHTag = Tag("</th>")
 
 	//OpenTDTag -- Open html td tag
-	OpenTDTag = Tag("<td>")
+	OpenTDTag = Tag("<td")
 	//CloseTDTag -- Close html td tag
 	CloseTDTag = Tag("</td>")
 
